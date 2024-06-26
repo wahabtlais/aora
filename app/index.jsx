e2247f5bf5439ecc0303cf7775f8fb13f5d61328
wahabtlais <hussientlais20@gmail.com>
2024-06-26 22:44:08 +0300

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function App() {
+	return (
+		<View className="flex-1 items-center justify-center bg-white">
+			<Text>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</View>
+	);
+}
