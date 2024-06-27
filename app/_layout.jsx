ef0b5ae1df71bac76d4240b76d1833b4509b8a97
wahabtlais <hussientlais20@gmail.com>
2024-06-27 22:35:47 +0300

    Updated app layout, added font loading, and removed profile component; updated dependencies and tailwind config. In addition of creating the onboard screen.

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,7 +1,43 @@
-import { Slot } from "expo-router";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
-  return <Slot />;
+	const [fontsLoaded, error] = useFonts({
+		"Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+		"Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+		"Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+		"Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+		"Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+		"Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+		"Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+		"Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+		"Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+	});
+
+	useEffect(() => {
+		if (error) throw error;
+
+		if (fontsLoaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [fontsLoaded, error]);
+
+	if (!fontsLoaded) {
+		return null;
+	}
+
+	if (!fontsLoaded && !error) {
+		return null;
+	}
+
+	return (
+		<Stack>
+			<Stack.Screen name="index" options={{ headerShown: false }} />
+		</Stack>
+	);
 };
 
 export default RootLayout;
